aa9d0404d70617804008ed7f064066d8d129ccff
Lovro Santek <lovro.santek29@gmail.com>
2025-03-06 17:15:43 +0100

    Add IMU driver

--- main/main.c
+++ main/main.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
+#include <IMU_driver.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_log.h"
+
+#define SAMPLE_FREQUENCY (10.f)
+#define SAMPLE_PERIOD (1.f / SAMPLE_FREQUENCY)
 
 void app_main(void)
 {
-    printf("Hello world!\n");
-    while(1);
+    IMU_init();
+    while(1)
+    { 
+        float gx = IMU_read_gyro(GYRO_XOUT_H);
+        float gy = IMU_read_gyro(GYRO_YOUT_H);
+        float gz = IMU_read_gyro(GYRO_ZOUT_H);
+        float ax = IMU_read_accel(ACCEL_XOUT_H);
+        float ay = IMU_read_accel(ACCEL_YOUT_H);
+        float az = IMU_read_accel(ACCEL_ZOUT_H);
+
+        ESP_LOGI("Acceleration: ", "x = %.3f  y = %.3f  z = %.3f", ax, ay, az);
+        //ESP_LOGI("Gyroscope: ", "x = %.3f  y = %.3f  z = %.3f", gx, gy, gz);
+
+        vTaskDelay((int) (1000 * SAMPLE_PERIOD) / portTICK_PERIOD_MS);
+    }
     return;
 }
